fafa0451c7cc7ae44847d0dc6ea75bcddd3ecd8c
chenyong <chenyong@mangatoon.mobi>
2023-11-14 11:05:55 +0800

    log

--- api/chatgpt/api.go
+++ api/chatgpt/api.go
@@ -59,6 +59,7 @@ func CreateConversation(c *gin.Context) {
 
 func sendConversationRequest(c *gin.Context, request CreateConversationRequest) (*http.Response, bool) {
 	jsonBytes, _ := json.Marshal(request)
+	print(jsonBytes)
 	req, _ := http.NewRequest(http.MethodPost, api.ChatGPTApiUrlPrefix+"/backend-api/conversation", bytes.NewBuffer(jsonBytes))
 	req.Header.Set("User-Agent", api.UserAgent)
 	req.Header.Set(api.AuthorizationHeader, api.GetAccessToken(c))
